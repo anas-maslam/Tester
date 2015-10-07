1569c6b5b1844f99cfd58afbc70b0a49977b5236
anas-maslam <anas.soclg@gmail.com>
2015-10-07 18:43:01 +0530

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,28 @@
+
+public class Tester {
+	int count = 0;
+	
+	public void increment()
+	{
+		int i = 0;
+		
+		for (i = 0; i< 10; i++)
+		{
+			count++;
+		}
+	}
+	
+	public void display()
+	{
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		Tester t = new Tester();
+		
+		t.increment();
+		t.display();
+		// TODO Auto-generated method stub
+		
+	}
+
+}
